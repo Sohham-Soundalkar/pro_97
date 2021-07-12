8bc7dd45e4b9e09196e9ede9d523725aabdaee07
Sohham-Soundalkar <74593541+Sohham-Soundalkar@users.noreply.github.com>
2021-07-12 09:04:28 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,10 @@
+ import random 
+ 
+ print("Number guessing game") 
+ # randint function to generate the random number between 1 to 9 
+ number = random.randint(1, 9) 
+ # number of chances to be given to the user to guess the number 
+ # # or it is the inputs given by user into input box here number of chances are 5 
+ chances = 0 
+ 
+ print("Guess a number (between 1 and 9):")
